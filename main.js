bdad4cb4c7ab9f03c66416b11cdbf95a80bb2b68
Ziyodilla <ziyodillajz@gmail.com>
2022-10-18 10:11:26 +0500

    Done

--- main.js
+++ main.js
@@ -4,9 +4,19 @@ var elBtn = document.querySelector(".btn");
 var elFizz = document.querySelector(".fizz");
 var elBuzz = document.querySelector(".buzz");
 var elFizzbuzz = document.querySelector(".fizzbuzz");
+var alertText = document.querySelector(".alert-js");
 var  elFizzArray = [];
 var  elBuzzArray = [];
 var  elFizzbuzzArray = [];
+
+function removeClass(){
+    alertText.classList.remove("add");
+    alertText.classList.add("remove");
+}
+function addClass(){
+    alertText.classList.add("add");
+    alertText.classList.remove("remove");
+}
 
 elForm.addEventListener("submit", function (evt) {
     evt.preventDefault();
@@ -16,15 +26,22 @@ elForm.addEventListener("submit", function (evt) {
             elFizzArray.push(elInputNumberValue);
             elFizz.textContent = elFizzArray;
             elInputNumber.value = "";
+            removeClass();
         }
         if(elInputNumberValue % 5 == 0){
             elBuzzArray.push(elInputNumberValue);
             elBuzz.textContent = elBuzzArray;
             elInputNumber.value = "";
+            removeClass();
         }
         if (elInputNumberValue % 3 == 0 && elInputNumberValue % 5 == 0){
             elFizzbuzzArray.push(elInputNumberValue);
             elFizzbuzz.textContent = elFizzbuzzArray;
+            elInputNumber.value = "";
+            removeClass();
+        }
+        if(elInputNumberValue % 3 != 0 && elInputNumberValue % 5 != 0){
+            addClass();
             elInputNumber.value = "";
         }
     }
